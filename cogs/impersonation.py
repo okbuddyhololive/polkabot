5a357eb78cb2cde67fffd1406b063e3b3307a624
bemxio <bemxiov@protonmail.com>
2022-05-03 19:25:06 +0200

    feat(cogs): improve opting commands

--- cogs/impersonation.py
+++ cogs/impersonation.py
@@ -55,11 +55,35 @@ class Impersonation(commands.Cog):
         await webhook.send(message, username=ctx.author.name, avatar_url=ctx.author.avatar_url)
         await session.close()
     
+    # opt in/out commands
+    @commands.command()
+    async def optin(self, ctx):
+        if ctx.author.id not in self.blacklist:
+            return await ctx.message.reply("You're already opted in!", mention_author=False)
+        
+        self.blacklist.remove(ctx.author.id)
+        await ctx.message.reply("You're now opted in!", mention_author=False)
+
     @commands.command()
     async def optout(self, ctx):
+        if ctx.author.id in self.blacklist:
+            return await ctx.message.reply("You're already opted out!", mention_author=False)
+        
+        def check(reaction, user):
+            return user == ctx.author and str(reaction.emoji) == "✅"
+        
+        message = await ctx.message.reply("Are you sure you want to opt out?\nReact with ✅ to confirm.", mention_author=False)
+        await message.add_reaction("✅")
+
+        try:
+            await self.bot.wait_for("reaction_add", timeout=60.0, check=check)
+        except TimeoutError:
+            return await message.edit(content="Didn't get a reaction in time, so you're still opted in.")
+        
         await self.messages.remove(ctx.author)
         self.blacklist.append(ctx.author.id)
-        await ctx.message.reply("Successfully deleted all message data from you and added you to the log blacklist!", mention_author=False)
+
+        await message.edit(content="Successfully deleted all message data from you and added you to the log blacklist!", mention_author=False)
 
 def setup(bot: commands.Bot):
     bot.add_cog(Impersonation(bot))
