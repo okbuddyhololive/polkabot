963bf1dad984d822c77ab46d64b7a344dddc18f4
bemxio <49116762+bemxio@users.noreply.github.com>
2022-08-30 19:59:56 +0200

    feat(cogs): add a `pol top` command + other minor fixes (#36)
    * feat(cogs): add a `pol user` command
    
    * fix(cogs): clean up the embeds
    
    * fix(cogs): delete the mention feature from `pol count`
    
    * feat(cogs): add a stopword list
    
    * feat(cogs): do not count emotes
    
    * refactor(cogs): change the name from user to top
    
    * docs(cogs): write a help message for pol top
    
    * fix: remove the dot from the prefix list

--- cogs/statistics.py
+++ cogs/statistics.py
@@ -2,28 +2,32 @@ from discord.ext import commands
 from discord import Embed, Colour, User
 from discord import NotFound
 
-from typing import Union
+from typing import Optional, List
 
 from modules.chain import MessageManager
 
 class Statistics(commands.Cog):
     def __init__(self, bot: commands.Bot, messages: MessageManager):
         self.bot = bot
-        self.messages = messages
 
+        self.messages = messages
+        self.stopwords = self.bot.config["Commands"]["User"]["stopwords"]
+
+    # helper functions
+    def word_split(self, text: str) -> List[str]:
+        content = text.lower()
+
+        return content.split()
+    
+    # actual commands
     @commands.command()
-    async def count(self, ctx: commands.Context, *, content: Union[User, str]):
+    async def count(self, ctx: commands.Context, *, keyword: str):
         """
-        Counts the amount of messages containing a keyword and shows the Top #10 people who said it.
+        Counts the amount of messages containing a keyword and shows the Top 10 people who said it.
 
         **Arguments:**
         - `keyword`: The keyword to search for.
         """
-
-        if isinstance(content, str):
-            keyword = content.lower()
-        else:
-            keyword = f"@{content.name}"
         
         occurences = {}
 
@@ -33,6 +37,7 @@ class Statistics(commands.Cog):
         for message in messages:
             text = message.get("content")
             text = text.lower()
+
             author = message["author"]["id"]
             
             if author not in occurences:
@@ -44,8 +49,7 @@ class Statistics(commands.Cog):
         occurences = dict(sorted(occurences.items(), key=lambda occurence: occurence[1], reverse=True))
 
         embed = Embed(
-            title=f"Top #10 users who've said '{keyword}':", 
-            #description="(based on message data collected here thus far)",
+            title=f"Top #10 users who've said \"{keyword}\":", 
             colour=Colour.blurple(),
             timestamp=ctx.message.created_at
         )
@@ -70,6 +74,60 @@ class Statistics(commands.Cog):
             if index == 10: # 10th place
                 break
             
+            index += 1
+        
+        await ctx.message.reply(embed=embed, mention_author=False)
+
+    @commands.command()
+    async def top(self, ctx: commands.Context, target: Optional[User] = None):
+        """
+        Counts the amount for each word that a user has said and shows the Top 10 words.
+
+        **Arguments:**
+        - `target`: The user that will be analyzed.
+        """
+        
+        target = target or ctx.author
+        occurences = {}
+
+        async with ctx.typing():
+            messages = await self.messages.get(target)
+
+        for message in messages:
+            for word in self.word_split(message.get("content", "")):
+                if word in self.stopwords:
+                    continue
+
+                if word.startswith(":") and word.endswith(":"):
+                    continue
+
+                if word not in occurences:
+                    occurences[word] = 0
+
+                occurences[word] += 1
+        
+        occurences = dict(sorted(occurences.items(), key=lambda occurence: occurence[1], reverse=True))
+
+        embed = Embed(
+            title=f"Top #10 words said by {target.display_name}:", 
+            colour=Colour.blurple(),
+            timestamp=ctx.message.created_at
+        )
+
+        embed.set_footer(text=f"Invoked by {ctx.author}", icon_url=ctx.author.avatar_url)
+        embed.set_thumbnail(url=target.avatar_url)
+
+        index = 1
+
+        for word, count in occurences.items():
+            embed.add_field(
+                name=f"#{index} - \"{word}\"",
+                value=f"**{count}** uses",
+            )
+
+            if index == 10: # 10th place
+                break
+
             index += 1
         
         await ctx.message.reply(embed=embed, mention_author=False)
