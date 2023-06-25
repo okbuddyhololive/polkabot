a4e3efa131e8da3d391a2f963d55bb60329400e5
bemxio <bemxiov@protonmail.com>
2023-06-25 13:58:36 +0200

    fix the update script not moving into the polkabot code directory

--- update.sh
+++ update.sh
@@ -4,6 +4,9 @@
 sudo -S apt-get update
 sudo apt-get upgrade -y
 
+# move into the polkabot directory
+cd ~/project-pretender
+
 # update python packages
 python3 -m pip install -r requirements.txt --upgrade
 
