e515e7661783b217db7b14bff4defc9f1c7f9ab7
bemxio <bemxiov@protonmail.com>
2024-12-04 21:36:41 +0100

    add attachment URLs to the database as well

--- modules/chain.py
+++ modules/chain.py
@@ -2,7 +2,7 @@ import re
 from typing import Dict, List, Union
 
 import markovify
-from discord import Member, Message, User
+from discord import Attachment, Member, Message, User
 from motor.motor_asyncio import AsyncIOMotorDatabase
 
 class MessageManager:
@@ -16,14 +16,10 @@ class MessageManager:
         self.tries = tries
 
     async def get(self, user: Union[Member, User]) -> List[Dict]:
-        cursor = self.collection.find({"author": {"id": str(user.id)}})
-
-        return await cursor.to_list(length=None)
+        return await self.collection.find({"author": {"id": str(user.id)}}).to_list(length=None)
 
     async def default(self) -> List[Dict]:
-        cursor = self.collection.find({})
-
-        return await cursor.to_list(length=self.max_limit)
+        return await self.collection.find({}).to_list(length=self.max_limit)
 
     async def links(self, extensions: List[str]) -> List[Dict]:
         cursor = self.collection.aggregate([
@@ -57,14 +53,19 @@ class MessageManager:
 
         return await cursor.to_list(length=None)
 
-    async def add(self, message: Message):
-        return await self.collection.insert_one({
+    async def add(self, message: Message) -> None:
+        content = message.clean_content
+
+        if message.attachments:
+            content += " " + " ".join(attachment.url for attachment in message.attachments)
+
+        await self.collection.insert_one({
             "author": {"id": str(message.author.id)},
-            "content": message.clean_content
+            "content": content
         })
 
-    async def remove(self, author: Union[Member, User]) -> List[str]:
-        return await self.collection.delete_many({"author": {"id": str(author.id)}})
+    async def remove(self, author: Union[Member, User]) -> None:
+        await self.collection.delete_many({"author": {"id": str(author.id)}})
 
     async def generate(self, author: Union[Member, User]) -> str:
         dataset = await self.get(author)
