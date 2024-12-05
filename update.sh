5bef0a2c1d984e93ea15efcc6e4dc476bb65358c
bemxio <bemxiov@protonmail.com>
2024-12-05 22:29:55 +0100

    activate the venv inside the update script

--- update.sh
+++ update.sh
@@ -6,8 +6,8 @@ sudo -S apt-get update && sudo apt-get upgrade -y
 # move into the polkabot directory
 cd ~/polkabot
 
-# update python packages
-python3 -m pip install -r requirements.txt --upgrade
+# activate the virtual environment and update Python packages
+source .venv/bin/activate && python3 -m pip install -r requirements.txt --upgrade
 
 # pull the latest code from the repository
 git pull origin main
